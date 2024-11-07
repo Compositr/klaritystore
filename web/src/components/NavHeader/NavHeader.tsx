b2538650127692603c7346cb072bf259f2309c1e
Compositr <compositr@compositr.dev>
2024-11-07 14:45:38 +1100

    feat: logo and some header buttons
    Added logo and search, cart and menu buttons to NavHeader

--- web/src/components/NavHeader/NavHeader.tsx
+++ web/src/components/NavHeader/NavHeader.tsx
@@ -1,6 +1,36 @@
+import { Menu, Search, ShoppingCart } from 'lucide-react'
+import logoLightSrc from 'public/img/logo-light.png'
+
+import { Link, routes } from '@redwoodjs/router'
+
+import { Button } from '../ui/Button'
+
 const NavHeader = () => {
   return (
-    <header>
+    <header className="w-full border-b border-b-border">
+      <div className="container flex items-center justify-center gap-1">
+        {/* Left Side Buttons */}
+        <div className="flex justify-around gap-2">
+          <Button variant="outline" size="icon">
+            <Search />
+          </Button>
+        </div>
+        {/* Logo */}
+        <div className="flex flex-1 justify-center self-center">
+          <Link to={routes.home()}>
+            <img src={logoLightSrc} alt="logo" width={262 / 2} />
+          </Link>
+        </div>
+        {/* Right Side Buttons */}
+        <div className="flex justify-around gap-2">
+          <Button variant="outline" size="icon">
+            <ShoppingCart />
+          </Button>
+          <Button variant="outline" size="icon" className="lg:hidden">
+            <Menu />
+          </Button>
+        </div>
+      </div>
       <nav></nav>
     </header>
   )
