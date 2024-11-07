5ff360798027dcbcc61aa5e0df5d79c58538134a
Compositr <compositr@compositr.dev>
2024-11-08 10:57:56 +1100

    feat: new HomePage section

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,23 @@
 import { Metadata } from '@redwoodjs/web'
 
 import Hero from 'src/components/Hero/Hero'
+import { H1 } from 'src/components/ui/typography/H1'
+import P from 'src/components/ui/typography/P'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
       <Hero />
+      <div className="container my-16 flex flex-col items-center justify-center">
+        <H1>
+          Bring some <span className="font-logo font-normal">Klarity</span> to
+          Your Schedule
+        </H1>
+        <P>
+          Shop our exclusive range of planners, journals and notepads today!
+        </P>
+      </div>
     </>
   )
 }
