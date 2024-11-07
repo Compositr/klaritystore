670cafc8423794b1b2897f86c851e92ca695a67c
Compositr <compositr@compositr.dev>
2024-11-07 22:38:12 +1100

    feat: serif fonts on headings

--- web/src/components/ui/typography/H1.tsx
+++ web/src/components/ui/typography/H1.tsx
@@ -1,6 +1,6 @@
 export function H1({ children }: React.PropsWithChildren) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="scroll-m-20 font-serif text-4xl font-extrabold tracking-tight lg:text-5xl">
       {children}
     </h1>
   )
