c4b374063597e133d3662c2cd1be7d8aac001bf7
Compositr <compositr@compositr.dev>
2024-11-15 09:34:43 +1100

    feat(web): add adminpage placeholders

--- web/src/pages/AdminPage/AdminPage.tsx
+++ web/src/pages/AdminPage/AdminPage.tsx
@@ -1,19 +1,27 @@
-// import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import H3 from 'src/components/ui/typography/H3'
 
 const AdminPage = () => {
   return (
     <>
-      <Metadata title="Admin" description="Admin page" />
-
-      <h1>AdminPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AdminPage/AdminPage.tsx</code>
-      </p>
-      {/*
-          My default route is named `admin`, link to me with:
-          `<Link to={routes.admin()}>Admin</Link>`
-      */}
+      <Metadata
+        title="Admin"
+        description="Administration and management interface"
+      />
+      <H3>Management Interface</H3>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="aspect-video rounded-xl bg-muted">stat card</div>
+          <div className="aspect-video rounded-xl bg-muted">stat card</div>
+          <div className="aspect-video rounded-xl bg-muted">stat card</div>
+        </div>
+        <div className="grid auto-rows-min gap-4 md:grid-cols-2">
+          <div className="aspect-video rounded-xl bg-muted">graphs</div>
+          <div className="aspect-video rounded-xl bg-muted">graphs</div>
+        </div>
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
     </>
   )
 }
