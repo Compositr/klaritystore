97c9d768069016243d449880c2a532aa8e9516b8
Compositr <compositr@compositr.dev>
2024-11-18 19:22:00 +1100

    build(web): add shadcn toasts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,9 +6,9 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
 import { AuthProvider, useAuth } from './auth'
-
 import './index.css'
 import './fonts.css'
+import { Toaster } from './components/ui/Toaster'
 
 interface AppProps {
   children?: ReactNode
@@ -18,7 +18,10 @@ const App = ({ children }: AppProps) => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
-        <RedwoodApolloProvider useAuth={useAuth}>{children}</RedwoodApolloProvider>
+        <RedwoodApolloProvider useAuth={useAuth}>
+          {children}
+        </RedwoodApolloProvider>
+        <Toaster />
       </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
